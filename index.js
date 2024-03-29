c2218dea3c2418cc97b649861c7d5951eddb657c
kamlesh <kamleshchandel17@gmail.com>
2024-03-29 15:29:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+let referralList = [{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/d1c8/4f81/48cf1bd48fa2c88ab68fa747c1be05c9?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aOM5I4uLSwaDgomYPcfTEBmmfXciGwbZIVggbaMcQ5svMol4zU745apqdI56SvNfR5-Jmv2zJfOh95ru7uai7fhinwnJ2wKJUdLlGriDMNrJnSV1d4vWOuchOTFhmtLj8ES4qdRY2QKJ5IDzYYQGh4GS5~yBDHR1oRqOK9Q0~oJTEhz8HGDhV-d1wNwaToqUdsqRTbCBqGr2lkJ76486lAw6utVCB78ivk3LnMFy3oZPn27Ric4X1nMf3oyjOj5CfDLAuZ4VXF3CC7G1l5N-Cs~3Nn-w4Dt29WZPhq3mxW7ltAmiziJqyLKPWSDEVrjH1XppLqNDqt69kDQTwlTFMw__",
+    para1: "$9,568.00",
+    para2: "Total Balance",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/d1c8/4f81/48cf1bd48fa2c88ab68fa747c1be05c9?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=aOM5I4uLSwaDgomYPcfTEBmmfXciGwbZIVggbaMcQ5svMol4zU745apqdI56SvNfR5-Jmv2zJfOh95ru7uai7fhinwnJ2wKJUdLlGriDMNrJnSV1d4vWOuchOTFhmtLj8ES4qdRY2QKJ5IDzYYQGh4GS5~yBDHR1oRqOK9Q0~oJTEhz8HGDhV-d1wNwaToqUdsqRTbCBqGr2lkJ76486lAw6utVCB78ivk3LnMFy3oZPn27Ric4X1nMf3oyjOj5CfDLAuZ4VXF3CC7G1l5N-Cs~3Nn-w4Dt29WZPhq3mxW7ltAmiziJqyLKPWSDEVrjH1XppLqNDqt69kDQTwlTFMw__",
+    para1: "60%",
+    para2: "Discount Percentage",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/7aed/034e/fbe0f928f9ec5fd36a975cc9cfaa212c?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DYKQL2kH6-2jE-HGaNPLKglKaBjR3jvqhHxx8XrJBxHEAlE~ZYC1tAGbkwnNGZaiK1XF0PlPqCHbQvnY-ulIy~a5utX8Y1MTYrVINgu3kxMg5tMRYPQ2gQuSzGpzTqIzfEUC~jaVkQ0ImgexycDwS8BolUk1NVa6~omB3pV6ACN2YfNNDW-sAq4Mac0shvk522fkQ5LlYmVx~76JXXLChVPSiFQoi7KQHi~B-e15f36SV4VxquE5Gfv8gBJHKpn9ntnpkUiDzEKLNOti2R9ro1O7G9cKaRTo9t0I0xiUFvCRmVqInSr-78ka~lqj798V-LnTW4rDfdugZF~uGLrFtA__",
+    para1: "300",
+    para2: "Total Referral",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/6708/27d5/01ba36d7f6cc61a4a434693b0521fcbf?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UB-Go-uxMLLOLbMSBSmz9DKq9fSMpMmtJwzTFzWnmOfCH7co5k8TKHtlY4aZDi1dGee2b25nj4UK1C1vrCj5R1nZj8~inULZY8B4VmRUuTz5qWC6b-DJK5ws5ks2tBkR1d-8Wskpg69gNn9R~ZHLAYgZX1oaeI0oi~dOFIt0vegdCpplUd9y-aBhbA4E1NVUBKticinGy0SQxIyTw8bE4-8wiX6PVhlLsM2E5rNi3OvnhA~ytE~YtlsySCWtavgccMhTnkhjJHxU4jt9WTnFMCNHgKVITUhbRKC6CYoAJIbACNEr2zaJLumyXEShLR1tQk1n1PXfTyvq-gJiMA9DtA__",
+    para1: "$300",
+    para2: "Discount Amount",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/1f2a/5e9f/3d9b87ae0c22a4aed2fb1be4e047eaef?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gw0NDYCa9mczWfxybWLnCLzQz7q2ExgvBUyqg~-CnlC-UxYmsKEeWFdmot2pdcISmBZCePSPsT~ZDJU~4fxoboOA7ZYdcvPnlMy0pmGawpYMtlG9v3gkD0iBUyJIWdl62nSB6KGY9mWplrm0eNduuKSRpu-9pgNO5ywnOVSGjVHevh9TxoEgSmaXdi15Zri8eMJzrcc~NsWcFzSKxhr8FHFw2CjBr1d0922d2zgZ8BlDcOmoxKDULnMonxJfou3BLgVxBXMZWPiErKj~1CxLlK4DjiajlDVg1Of1lBuH-UD4q9erqDYOb8-R8hMLy7o0mQgrCJJgkehxcyH5t9xMzQ__",
+    para1: "$465.00",
+    para2: "Commission Amount",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/567a/01ac/adfd2d5583bdeea46241711cec1f847d?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CG93EtteH08lV1jjtVEVRsNTDkV27JxHD8tYtFL6fvyIAvusSMhPIFcQj~mg~cOzP-jpJncUw9vxr6aHu55Sll1WtpYAh-HW-UqE24T00gf0FRVGDL26ZopJmtGm9v4HLCbHqBGmwZvcReXfPV-Kbb~S2Vp7cvDwm~lTHNDXCn7ah~F3vcUne-OrSLkBmRL5ThsFdyxikBGIJWuX-SG8HeWBJQHA0pui~X9Yg379um~TLkLVj1QeM8n9snY5f9UaBryJwvsu3Gt-2CIKnWlaiSq7d~fzOT1NWNbaPKT-O1QQZggsxF6dPrizTSXaivVx8VNRxUpL1Z22nJ6kSny4lQ__",
+    para1: "$158.00",
+    para2: "Total Earning",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/c03d/a50a/01463f25a8ada594ceaa0f6266ba8097?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=D2xQqoDPWJ~ta24H5n-YvFdUlZqmg5Y0akTTud0MM98bV50VaxgoUlDcnUuum3n21p14L-2OKq0mAcxUTdwm8c8vfPD1-MZE5uoGMrLlFcb-wXDx6JuwqWxzMGVP5Gvy2dc~Dk5KHYiwKVDsxYNu7LeaeHFIEwTAAJs5~l-1LbCYLqwwM8uzJE8-l3GMBbbk0n2rFpwr0Qt8P6rqJKXCayFK9OpvLEF1qHBHf2M7l41FZr6XbWDkFJCVoNW~llDoCv6SjQya3f~-gTsnZkR-9sYowfv7fNWD1rrdzKe~l17opHK8phygYjrfXsOmyALdmt09gNCg9AQHOHW-APEfhQ__",
+    para1: "40%",
+    para2: "Commission Discount",
+},
+{
+    imgUrl: "https://s3-alpha-sig.figma.com/img/8ce7/180a/26425d2810f6febcdff88ca6d9bf0d51?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=E0v~XjPxdYxw5-7hvAsLI2Q3HXK1wdE~ACp2LmbIKTBd9ibXgW1DN3wAg-fGNZ-IIgGvWC8U~3zGJ99YH6ZrBD0Mdx2egu-DCgUrWilVsEg91uP053KE-5C6pl-ts8CX7wWiR02od7h6YW9X8h~HqAhPomSyJMlVwwqHHRD2QRE7WGPJISheptK8sU69-4Nj8KIV0ELEr0R~db1Pmxk9L12SmjEvzq3B4k4OoKAmy6FrePsZCh33teom~QnXvZ~76G7RZU07kB7XZh8iM-rW0gPORt~q7qs-mS4WVR891PAAemxzUPSiDee~LlwitalfadrKPp~i0A1FC0H0iLe4DA__",
+    para1: "$534.00",
+    para2: "Total Bank Transfer",
+},
+];
+let summaryList = [{
+    type: "Service",
+    number: "some service",
+},
+{
+    type: "Your Referrals",
+    number: "0 +   0",
+},
+{
+    type: "Active Referrals",
+    number: "0 +   0",
+},
+{
+    type: "Total Ref. Earnings",
+    number: "$0.00",
+},
+]
+referralList.map(eachList => {
+let listContainer = document.getElementById("list");
+let referralItem = document.createElement("li");
+referralItem.classList.add("listItem");
+listContainer.appendChild(referralItem);
+let imgElement = document.createElement("img");
+imgElement.src = eachList.imgUrl;
+imgElement.classList.add("listImg");
+referralItem.appendChild(imgElement);
+let paraDiv = document.createElement("div");
+referralItem.appendChild(paraDiv);
+let para1Element = document.createElement("p");
+para1Element.textContent = eachList.para1;
+para1Element.classList.add("para1");
+paraDiv.appendChild(para1Element);
+let para2Element = document.createElement("p");
+para2Element.textContent = eachList.para2;
+para2Element.classList.add("para2");
+paraDiv.appendChild(para2Element);
+});
+
+summaryList.map(each => {
+let summaryDiv = document.getElementById("top-4-div");
+let summaryItem = document.createElement("div");
+summaryItem.classList.add("summaryItem")
+summaryDiv.appendChild(summaryItem);
+let summaryPara1 = document.createElement("p");
+summaryPara1.textContent = each.type;
+summaryItem.appendChild(summaryPara1);
+let summaryPara2 = document.createElement("p");
+summaryPara2.textContent = each.number;
+summaryPara2.classList.add("number");
+summaryItem.appendChild(summaryPara2)
+})
